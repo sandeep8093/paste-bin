66ae3a9d7fee7cea886f7f9d0c89cea1805e5d88
Sandeep Kumar Nayak <57430805+sandeep8093@users.noreply.github.com>
2022-02-18 10:29:12 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,15 @@ import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 
 import './index.css';
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-  
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
-
-
